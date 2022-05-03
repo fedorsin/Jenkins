2e07e2d58826b7f69757357fdcc59b9772c1d98c
fedorsin <93473001+fedorsin@users.noreply.github.com>
2022-05-03 10:40:01 +0500

    Update test_py.py

--- test_py.py
+++ test_py.py
@@ -24,4 +24,4 @@ def test_sum_clients(): # Тест на наличие 10 строк в Clients
 def test_sum_orders(): # Тест на наличие 10 строк в Orders
     assert len(app.Orders.select()) > 9
 def test2():
-    assert true
+    assert True
